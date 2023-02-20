90fc269433a9acaf45f9db4cc40c3d9b8f3e2912
Denis2982 <denisdenis2982@gmail.com>
2023-02-20 13:16:32 +0200

    init repository and added files

---
+++ script.js
@@ -0,0 +1,6 @@
+function burgerNav() {
+    var listBurger = document.getElementById('listBurger');
+    var menuBurger = document.getElementById('menuBurger');
+    listBurger.classList.toggle('active');
+    menuBurger.classList.toggle('active');
+}
